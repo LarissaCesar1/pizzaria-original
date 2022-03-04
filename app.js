c39b57212c9472691e7bb41fcd02693d42f26e14
Larissa Cesar <larissavitoriacesar1415@gmail.com>
2022-03-04 17:43:49 -0300

    Fase 3 concluida

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const PizzasRouter = require('./routes/PizzasRouter');
 
 app.get('/', (req, res) => {
     res.send("Olá visitante!")
---
+++ routes/PizzasRouter.js
@@ -0,0 +1,4 @@
+const express = require('espress');
+const router = express.router();
+module.exports = {router};
+
