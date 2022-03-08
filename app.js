c96801843d9049c6cde32996ba149e0956306256
Larissa Vitória <91913482+LarissaCesar1@users.noreply.github.com>
2022-03-08 16:30:35 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
-const app = express(); 
-const PizzasRouter = require('./routes/PizzasRouter');
+
+const app = express();
+
+const porta = 3000;
+
+const Pizzarouter = require('./routes/Pizzarouter');
 
 app.get('/', (req, res) => {
-    res.send("Olá visitante!")
-    console.log("Servidor rodando na porta 3000")
-})
-app.use('/',PizzasRouter); 
-app.listen(3000);
+    res.send('Olá, visitante!')
+});
+
+const path = require ('path')
+app.set('view engrine', 'ejs')
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(porta, () => {
+    console.log(`Servidor Rodando na porta ${porta}`)
+});
